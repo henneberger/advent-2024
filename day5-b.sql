27ab81410d692f30604461ff486bb8f9f131f6d9
Daniel Henneberger <git@danielhenneberger.com>
2024-12-05 14:40:29 -0800

    Remove unnecessary topo sort items, improve aesthetics
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- day5-b.sql
+++ day5-b.sql
@@ -24,36 +24,29 @@ create table updates (
   'csv.ignore-parse-errors' = 'true'
 );
 
--- Rule violations
+-- rule violations
 create temporary view a as
-select line, max(u_ts) as ts
-from rules as r
-left join updates as u on array_position(line, l) > array_position(line, r)
- and array_position(line, l) <> 0 and array_position(line, r) <> 0
-group by line;
+select distinct line
+from rules r
+join updates u on array_position(line, r.l) > array_position(line, r.r)
+               and array_position(line, r.l) <> 0
+               and array_position(line, r.r) <> 0;
 
 -- Get rules for each line
 create temporary view b as
-select line, r.l as dependency, r.r as page
-from a, rules r
-where array_position(line, l) <> 0 and array_position(line, r) <> 0;
+select a.line, r.l as dependency, r.r as page
+from a
+join rules r on array_position(line, r.l) <> 0
+             and array_position(line, r.r) <> 0;
 
--- Prepare for topo sort
-create temporary view unique_pages as
-select distinct l as page from rules
-union
-select distinct r as page from rules;
-
--- Number the dependencies
+-- number the dependencies
 create temporary view c as
 select
     b.line,
-    b.page as page,
+    b.page,
     count(distinct b.dependency) + 1 as sort_key
 from b
-right join unique_pages p on p.page = b.page
-group by b.line, b.page
-having b.line is not null;
+group by b.line, b.page;
 
 -- find max so we can find the midpoint
 create temporary view d as
@@ -61,12 +54,12 @@ select line, max(sort_key) as m
 from c
 group by line;
 
--- find the midpoint value and sum
+-- find the midpoint value sum
 create temporary view e as
 select sum(page) as total
 from c
 join d on c.line = d.line
-where (m+1)/2 = sort_key;
+where (m + 1) / 2 = sort_key;
 
 create table print_sink (
   total bigint
