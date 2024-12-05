f6458ae5424eed66bbaba410b4dcae45f8c7a8fd
Daniel Henneberger <git@danielhenneberger.com>
2024-12-05 01:00:55 -0800

    WIP for day 5-b
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

---
+++ day5-b.sql
@@ -0,0 +1,52 @@
+add jar '/users/henneberger/advent-of-code/target/aoc-flink-lib-1.0-SNAPSHOT.jar';
+
+create temporary function if not exists topo_sort
+  as 'io.github.henneberger.TopoSort' language java;
+
+-- step 1: create the rules table
+create table rules (
+    l int,
+    r int,
+    r_ts as proctime()
+) with (
+  'connector' = 'filesystem',
+  'path' = '/Users/henneberger/advent-of-code/data/day5-input-a.txt',
+  'format' = 'csv',
+  'csv.field-delimiter' = '|',
+  'csv.ignore-parse-errors' = 'true'
+);
+
+-- step 3: create the updates table
+create table updates (
+    o String,
+    line AS CAST(split(o, ',') as array<int>),
+    u_ts as proctime()
+) with (
+   'connector' = 'filesystem',
+   'path' = '/Users/henneberger/advent-of-code/data/day5-input-b.txt',
+   'format' = 'csv',
+   'csv.field-delimiter' = '|',
+   'csv.ignore-parse-errors' = 'true'
+ );
+
+-- Rule violations
+create temporary view a as
+select line, array_agg(array[r.l, r.r]) as arr, max(u_ts) as ts
+from rules as r
+left join updates as u on array_position(line, l) > array_position(line, r)
+ and array_position(line, l) <> 0 and array_position(line, r) <> 0
+group by line;
+
+create temporary view b as
+select sum(line[cardinality(line)/2+1]) as total
+from a;
+
+create table print_sink (
+  total bigint
+) with (
+  'connector' = 'print'
+);
+
+insert into print_sink
+select total
+from b;
---
+++ src/main/java/io/github/henneberger/TopoSort.java
@@ -0,0 +1,37 @@
+package io.github.henneberger;
+
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.ImmutableGraph;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.flink.table.functions.ScalarFunction;
+
+public class TopoSort extends ScalarFunction {
+
+  public List<Integer> eval(List<List<Integer>> pairs) {
+    ImmutableGraph.Builder<Integer> graphBuilder = GraphBuilder.directed().immutable();
+    for (List<Integer> edge : pairs) {
+      graphBuilder.putEdge(edge.get(0), edge.get(1));
+    }
+    return topologicalSort(graphBuilder.build());
+  }
+
+  public static List<Integer> topologicalSort(ImmutableGraph<Integer> g) {
+    Map<Integer, Integer> d = new HashMap<>();
+    for (Integer n : g.nodes()) {
+      d.put(n, 0);
+    }
+    for (Integer n : g.nodes()) {
+      for (Integer s : g.successors(n)) {
+        d.put(s, d.get(s) + 1);
+      }
+    }
+
+    return d.entrySet().stream()
+        .sorted(Map.Entry.comparingByValue())
+        .map(Map.Entry::getKey)
+        .collect(Collectors.toList());
+  }
+}
