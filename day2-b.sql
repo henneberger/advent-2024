b3715e3e0f05019775aa96b9e36f74c911630122
Daniel Henneberger <git@danielhenneberger.com>
2024-12-04 17:26:00 -0800

    Improve strategy for day 2-b
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- day2-b.sql
+++ day2-b.sql
@@ -1,9 +1,10 @@
 -- We have to maintain a uuid to keep partitions unique, also flink has no simple way to remove items
 -- from arrays consistently.
 
-CREATE TABLE input_table (
-  item STRING,
-  ts AS PROCTIME()
+create table input_table (
+  parent_item STRING,
+  ts AS proctime(),
+  parent_uuid as UUID()
 ) WITH (
   'connector' = 'filesystem',
   'path' = '/Users/henneberger/advent-of-code/data/day2-input.txt',
@@ -13,54 +14,61 @@ CREATE TABLE input_table (
 );
 
 -- create enough entries to enumerate
-CREATE TEMPORARY VIEW a AS
-SELECT cast(split(item, ' ') as array<bigint>) as item, ts, ROW_NUMBER() OVER(PARTITION BY item ORDER BY ts) AS rn
-FROM input_table i
-CROSS JOIN unnest(split(i.item, ' ')) as x;
+-- Note: add uuid b/c partitions can be duplicated
+-- Note: rn must be an 'int' for later array operations
+create temporary view a as
+select
+  cast(split(parent_item, ' ') as array<bigint>) as item,
+  cast(row_number() over(partition by parent_uuid order by ts) as int) as rn,
+  uuid() AS uuid, -- add a uuid
+  *
+from input_table i
+cross join unnest(split(i.parent_item, ' ')) as x;
 
 -- Create set to enumerate, we can exclude the original list since it'll be covered by the case where the ends are missing
-CREATE TEMPORARY VIEW b AS
-SELECT
- CASE
- WHEN rn = CARDINALITY(item) THEN array_slice(item, 2, CARDINALITY(item)) --remove first element
- ELSE array_concat(array_slice(item, 1, CAST(rn AS int)), array_slice(item, CAST(rn AS int)+2, CARDINALITY(item)))
- END AS item, item as original, ts, rn,  uuid() as uuid
-FROM a;
+-- Note: array_slice has odd behavior with removing the first element
+create temporary view b as
+select
+ case
+   when rn = cardinality(item) then array_slice(item, 2, cardinality(item)) --remove first element
+   else array_concat(array_slice(item, 1, rn), array_slice(item, rn + 2, cardinality(item)))
+ end as item,
+ uuid,
+ parent_uuid,
+ ts
+from a;
 
--- Unnest array
- --add another record b/c lag can span partitions
-CREATE TEMPORARY VIEW c AS
-SELECT /*+ STATE_TTL('i'='1s', x='1s') */
- cast(x as bigint) - lag(cast(x as bigint)) over (partition by uuid order by ts) as y,
- item, original, ts, uuid FROM b
-CROSS JOIN unnest(item) x;
+-- Convert to single elements
+create temporary view c as
+select
+  ele - lag(ele) over (partition by uuid order by ts) as diff,
+  item,
+  uuid,
+  parent_uuid,
+  ts
+from b
+cross join unnest(item) as ele;
 
--- [43, 40, 39, 35]
-
--- Keep it as a stream
-CREATE TEMPORARY VIEW d AS
-select /*+ STATE_TTL('a'='1s') */
- max(y > 3 or y = 0 or y < -3) OVER (partition by uuid order by ts) as unsafe,
- case when y > 0 then 1 else 0 end strict_asc,
- case when y < 0 then 1 else 0 end strict_desc,
+create temporary view d as
+select
+  case when
+   sum(case when diff >= -3 and diff < 0  then 1 else 0 end) over (partition by uuid order by ts) = cardinality(item) - 1 or
+   sum(case when diff > 0   and diff <= 3 then 1 else 0 end) over (partition by uuid order by ts) = cardinality(item) - 1
+  then true else false end as is_safe,
   *
 from c;
 
 create temporary view e as
-select count(distinct original) as total from (
-  select original, uuid, item, max(unsafe) as is_unsafe, sum(strict_asc), sum(strict_desc), count(*)
-  from d
-  group by original, uuid, item
-  having not max(unsafe) and (sum(strict_asc) = count(*) - 1 or sum(strict_desc) = count(*) - 1)
-);
+select count(distinct parent_uuid) as total
+from d
+where is_safe;
 
-CREATE TABLE print_sink (
-  total BIGINT
-) WITH (
+create table print_sink (
+  total bigint
+) with (
   'connector' = 'print'
 );
 
-INSERT INTO print_sink
-SELECT total
-FROM e;
-
+insert into print_sink
+select total
+from e;
