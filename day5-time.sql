b192d1bde5671525bda88180f24b39c061809071
Daniel Henneberger <git@danielhenneberger.com>
2024-12-05 19:15:24 -0800

    Add proper temporal semantics for day 5
    Signed-off-by: Daniel Henneberger <git@danielhenneberger.com>

--- day5-time.sql
+++ day5-time.sql
@@ -1,5 +1,8 @@
 -- Now that flink has 2 tables, we can start to work in temporal features
 
+-- We will make the assumption that if the rules change then we don't retroactively
+-- retract the updates.
+
 create table rules (
     event_time timestamp(3) not null,
     l int not null,
@@ -12,8 +15,6 @@ create table rules (
   'csv.ignore-parse-errors' = 'true'
 );
 
-
--- step 3: create the updates table
 create table updates (
     event_time timestamp(3) not null,
     o String,
@@ -27,59 +28,48 @@ create table updates (
    'csv.ignore-parse-errors' = 'true'
 );
 
--- Create a 'versioned table' for later temporal joins
--- Sets 'l' as the primary key to be join against
+-- Since we have to do a join, and joins are expensive in flink, we will
+-- use a temporal join to join a stateful 'rules' table with the updates table.
 create temporary view rules_distinct as
 select * from (
-   select l, event_time, arr,
-     row_number() over (partition by l order by event_time asc) as rownum
-   from (select *, array_agg(r) over (partition by l order by event_time asc) as arr from rules)
+   select l, event_time, arr, row_number() over (partition by l order by event_time asc) as rownum
+   from (
+     select *, array_agg(r) over (partition by l order by event_time asc) as arr
+     from rules)
    )
 where rownum = 1;
 
 -- unnest updates, but keep it as a stream
 create temporary view a as
-select *,
-row_number() over (partition by line order by event_time) as rn
+select *, row_number() over (partition by line order by event_time) as rn
 from updates u, unnest(u.line) as t(ele);
 
 -- temporal join against the current rules
 create temporary view b as
 select
-r.l,
-array_intersect(a.line, r.arr),
-cardinality(a.line) - cardinality(array_intersect(a.line, r.arr)) as i,
-case when abs(cardinality(a.line) - cardinality(array_intersect(a.line, r.arr)) - rn) = 0
-then 0
-else 1 end as is_not_safe,
---last_value(
-case when (cardinality(a.line) - cardinality(array_intersect(a.line, r.arr))) = (cardinality(a.line)+1)/2
-then l
-else null end
---) over (partition by a.line order by a.event_time)
- as mid,
-line[(cardinality(a.line)+1)/2] as mid2,
-coalesce(last_value(case when abs(cardinality(a.line) - cardinality(array_intersect(a.line, r.arr)) - rn) = 0
-           then null
-           else 1 end) over (partition by a.line order by a.event_time), 1) as safe_window,
-*
+  case when abs(cardinality(a.line) - cardinality(array_intersect(a.line, r.arr)) - rn) = 0
+       then 0
+       else 1 end as is_not_safe,
+  case when (cardinality(a.line) - cardinality(array_intersect(a.line, r.arr))) = (cardinality(a.line)+1)/2
+       then l
+       else null end as mid,
+  *
 from a
-join rules_distinct
-for system_time as of a.event_time
- as r on r.l = a.ele;
+join rules_distinct for system_time as of a.event_time as r on r.l = a.ele;
 
-
+-- tumble window since the event time on each update is the same
 create temporary view c as
 select line, max(mid) as m, sum(is_not_safe) as s
 from b
 group by tumble(event_time, interval '1' second), line;
 
--- problem 1
+-- solution to problem 1
 create temporary view p1 as
-select sum(m) from c where s = 0;
+select sum(m) as total from c where s = 0;
 
+-- solution to problem 2
 create temporary view p2 as
-select sum(m) from c where s > 0;
+select sum(m) as total from c where s > 0;
 
 create table print_sink (
   total bigint
@@ -87,6 +77,11 @@ create table print_sink (
   'connector' = 'print'
 );
 
-insert into print_sink
-select total
-from b;
+execute statement set begin
+  insert into print_sink
+  select total
+  from p1;
+  insert into print_sink
+  select total
+  from p2;
+end;
